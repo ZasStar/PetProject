ac179db1891054fd0d5ac2491cc7d73e4fb636a6
Nick <wizard228322@icloud.com>
2023-04-28 20:19:38 +0400

    Sign in block

---
+++ PetProject/ViewControllers/LogInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LogInViewController.swift
+//  PetProject
+//
+//  Created by 65 on 28.04.2023.
+//
+
+import UIKit
+
+final class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
