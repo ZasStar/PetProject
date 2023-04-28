ede7031960d51c4b9f81fb97e55b5002f3275bd3
Alex <zas_st@inbox.ru>
2023-04-28 11:53:41 +0300

    Initial Commit

---
+++ PetProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetProject
+//
+//  Created by Александр Захлыпа on 28.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
